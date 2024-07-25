e10fc8584a8fb4ed0715837d546b387337d8e145
yovxa <163300109+yovxa@users.noreply.github.com>
2024-07-26 00:18:38 +0300

    modified:   src/ResultPage.js

--- src/ResultPage.js
+++ src/ResultPage.js
@@ -55,7 +55,7 @@ function ResultPage() {
           <ul className={style.data}>
             {MockData.map((data) => (
               <li key={data.id}>
-                <a>{data.url}</a>
+                <a href={data.url}>{data.url}</a>
                 <br />
                 {data.description}
               </li>
